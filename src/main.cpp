7384841b3f76d07712f0d3977bef9bcff8b5b198
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-11-20 08:32:43 -0600

    Initiate a transaction for insert into data base

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,17 @@ int main()
     {
         SQLite::Database db("sqlite-example.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
         db.exec("DROP TABLE IF EXISTS test");
+
+        // Begin transaction
+        SQLite::Transaction transaction(db);
+
+        db.exec("CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)");
+
+        int nb = db.exec("INSERT INTO test VALUES (NULL, \"example test\")");
+        std::cout << "INSERT INTO test VALUES (NULL, \"example test\")\", returned " << nb << std::endl;
+
+        // Commit transaction
+        transaction.commit();
     }
     catch (std::exception& e)
     {
