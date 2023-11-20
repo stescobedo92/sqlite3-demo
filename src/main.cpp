988fbc91906ff5bb466c1b78de41ddd51c673649
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-11-20 08:30:07 -0600

    Adding baic structure for Project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-#include <utility>
+#include <SQLiteCpp/Database.h>
+#include <SQLiteCpp/Transaction.h>
 
 int main()
 {
-    
+    try
+    {
+        SQLite::Database db("sqlite-example.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
+
     return 0;
 }
